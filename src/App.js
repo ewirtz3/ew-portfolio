767efa0a48c2df34d5138e170924ebaf6a1832a2
Emily Wirtz <ewirtz3@gmail.com>
2021-03-07 16:18:40 -0600

    adding header component and MuiThemeProvider

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
 import React from "react";
+import MuiThemeProvider from "@material-ui/styles/ThemeProvider/ThemeProvider";
+import HeaderComponent from "./components/header/HeaderComponent";
 
 export default function App() {
-  return <div>Hello World</div>;
+  return (
+    <MuiThemeProvider>
+      <div className="root">
+        <div className="section header-section">
+          <div className="section-container header-component">
+            <HeaderComponent></HeaderComponent>
+          </div>
+        </div>
+      </div>
+    </MuiThemeProvider>
+  );
 }
