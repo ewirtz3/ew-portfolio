53ee9d443009bf853189d0b7cb7653bc3551fad9
Emily Wirtz <ewirtz3@gmail.com>
2021-04-18 16:56:24 -0500

    scroll to top works

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,21 +1,32 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Button from "@material-ui/core/Button";
+const scrollTo = require("scroll-to");
 
 const useStyles = makeStyles((theme) => ({
   root: {
     "& > *": {
       margin: theme.spacing(1),
     },
+    display: "flex",
+    flexWrap: "row wrap",
+    justifyContent: "center",
   },
 }));
 
 export default function Footer() {
   const classes = useStyles();
 
+  const onScrollToTop = () => {
+    scrollTo(0, 0, {
+      ease: "out-bounce",
+      duration: 2000,
+    });
+  };
+
   return (
-    <footer>
-      <Button href="#TOP" color="inherit">
+    <footer className={classes.root}>
+      <Button href="#TOP" color="inherit" onClick={onScrollToTop}>
         Back to top ^
       </Button>
     </footer>
