7b22b187d68306c10c657bbf7d0a683d1dcc4aa1
Emily Wirtz <ewirtz3@gmail.com>
2021-04-18 17:26:46 -0500

    socials displaying and linking properly

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import Button from "@material-ui/core/Button";
+import { Button, List, ListItem } from "@material-ui/core";
 const scrollTo = require("scroll-to");
 
 const useStyles = makeStyles((theme) => ({
@@ -12,10 +12,36 @@ const useStyles = makeStyles((theme) => ({
     flexWrap: "row wrap",
     justifyContent: "center",
   },
+  socials: {
+    display: "flex",
+    flexWrap: "row wrap",
+    justifyContent: "center",
+  },
 }));
 
 export default function Footer() {
   const classes = useStyles();
+
+  const socials = [
+    {
+      name: "GitHub",
+      hoverIcon: "",
+      hoverColor: "",
+      href: "https://github.com/ewirtz3",
+    },
+    {
+      name: "LinkedIn",
+      hoverIcon: "",
+      hoverColor: "",
+      href: "https://www.linkedin.com/in/emily-wirtz",
+    },
+    {
+      name: "Twitter",
+      hoverIcon: "",
+      hoverColor: "",
+      href: "https://twitter.com/emdotwirtz",
+    },
+  ];
 
   const onScrollToTop = () => {
     scrollTo(0, 0, {
@@ -29,6 +55,17 @@ export default function Footer() {
       <Button href="#TOP" color="inherit" onClick={onScrollToTop}>
         Back to top ^
       </Button>
+      <List className={classes.socials}>
+        {socials.map((site, i) => {
+          return (
+            <ListItem key={i}>
+              <a href={site.href} target="_blank">
+                {site.name}
+              </a>
+            </ListItem>
+          );
+        })}
+      </List>
     </footer>
   );
 }
