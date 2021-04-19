165e5cae32934d2bcdd5632507b20c6f138bca61
Emily Wirtz <ewirtz3@gmail.com>
2021-04-19 14:13:32 -0500

    added office video in contact section

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,5 +1,27 @@
 import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles(() => ({
+  root: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+}));
 
 export default function Contact() {
-  return <p>put something here</p>;
+  const classes = useStyles();
+  return (
+    <div className={classes.root}>
+      <iframe
+        width="560"
+        height="315"
+        src="https://www.youtube.com/embed/MwveKepqCos?start=187"
+        title="YouTube video player"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowfullscreen
+      ></iframe>
+    </div>
+  );
 }
